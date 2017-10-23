781628d0cfb3f3d75a0bba60b9a307b0c88c1f9d
zj <531847459@qq.com>
2017-10-23 23:53:15 +0800

    mybatis实现基础crud

---
+++ mybatis-base/src/main/java/cn/zj/mybatis/base/App.java
@@ -0,0 +1,13 @@
+package cn.zj.mybatis.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
